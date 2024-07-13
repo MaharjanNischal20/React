28a4a088d5c5e36d37bc0cdbb2807c3ce734366b
Nischal Maharjan <burninglykfire@gmail.com>
2024-07-13 11:04:34 +0545

    Learning React

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import Button from "./button"; 
+import Hello from "./Hello";
+
+function App() {
+  return (<div>
+    <h1>Hello My name is nischal</h1>
+    <Button />
+    <Hello></Hello>
+    </div>
+  )
+
+}
+
+export default App;
