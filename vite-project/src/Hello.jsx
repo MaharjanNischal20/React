ff8d1862c6e9a4aca1900565c49fac29a3483b53
MaharjanNischal20 <85216636+MaharjanNischal20@users.noreply.github.com>
2024-07-13 11:05:51 +0545

    Delete vite-project directory

--- vite-project/src/Hello.jsx
+++
@@ -1,8 +0,0 @@
-function Hello() {
-  let number = 456
-  let name = () => {
-    return "Dota 2"
-  }
-  return (<p>My hobby is to play games at free time {name()} at {number}</p>)
-}
-export default Hello;
